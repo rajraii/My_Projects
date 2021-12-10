8282deb9bf199cd2df20ee5d777f23b0e6a1e411
Raj Rai <rrai1590@gmail.com>
2021-12-10 12:32:06 +0530

    practice

---
+++ practice/practice.js
@@ -0,0 +1,25 @@
+// var area="circle";
+// var pi= 3.14, l=5,b=4,r=3;
+// switch (area) {
+//   case 'circle':
+//     console.log(pi*r**2);
+//     break;
+//   case 'rectangle':
+//       console.log(l*b);
+//   default:
+//     break;
+// }
+
+// ! Challenge
+
+// for(var num=1; num<=50;num++)
+//   console.log("8 * "+ num + "= "+ num*8);
+
+// ! Functions
+
+// function sum(){
+//   let a=5, b=10;
+//   console.log(a+b);
+// }
+// sum();
+// console.log(sum());
