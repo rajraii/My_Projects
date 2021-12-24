8ff615c4944121cb535ebea051ea52e9a539bcfb
Raj Rai <rrai1590@gmail.com>
2021-12-24 18:45:45 +0530

    changes

---
+++ practice/react/login_page/src/components/Login.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <>
+      <section className="login">
+        <div className="container mt-5">
+          <div className="signup-content">
+            <div className="signup-form">
+              <h2 className="form-title">login</h2>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
