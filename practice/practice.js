47d94c1a4f14e3fb8b4f9c0f2e415f4e70f2d7e1
Raj Rai <rrai1590@gmail.com>
2022-01-24 17:10:17 +0530

    JS practice 24/1/22

--- practice/practice.js
+++ practice/practice.js
@@ -22,3 +22,48 @@
 // }
 // sum();
 // console.log(sum());
+
+
+// for (var num=1; num<=10; num++){
+//   var tableof= 8;
+//   console.log(tableof +" * "+num +" = " +num*tableof);
+// }
+
+// function sum(a,b) {
+//   var total = a+b;
+//   console.log(total);
+// }
+
+// sum();
+// sum(10, 20);
+// sum(40,50);
+
+// function multi(a,b=5){
+//   return a*b;
+// }
+// console.log(multi(3));
+
+
+// var arr = ["raj",3,"name","yo"]
+// for(let i in arr){
+//   console.log(i);
+// }
+// for (const i of arr) {
+//   console.log(i);
+// }
+// arr.forEach((i,index,array) => {
+//   console.log(`${i} index at : ${index}`);
+// });
+
+// const price = [100,200,300,400,500,600];
+
+// const findelem = price.splice(5,1,900);
+// console.log(price);
+
+const price = [2,3,4,6,8];
+
+let newprice = price.map((curr) =>curr*2).filter((curr) =>  curr>10).reduce((accumulator, curr) => {
+  return accumulator+=curr;
+});
+
+console.log(newprice);
---
+++ practice/responsive-page/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import navbar from "./components/navbar/nav"
+
+export const App = () => {
+  return (
+    <div>
+      <navbar/>
+    </div>
+  )
+}
