95209536b1c4a0e0e2c2c2ce41d3ae209f089f2a
Raj Rai <rrai1590@gmail.com>
2022-02-02 12:54:22 +0530

    more practice on js

--- practice/practice.js
+++ practice/practice.js
@@ -60,10 +60,57 @@
 // const findelem = price.splice(5,1,900);
 // console.log(price);
 
-const price = [2,3,4,6,8];
+// const price = [2,3,4,6,8];
 
-let newprice = price.map((curr) =>curr*2).filter((curr) =>  curr>10).reduce((accumulator, curr) => {
-  return accumulator+=curr;
-});
+// let newprice = price.map((curr) =>curr*2).filter((curr) =>  curr>10).reduce((accumulator, curr) => {
+//   return accumulator+=curr;
+// },);
 
-console.log(newprice);
+// console.log(newprice);
+
+// let fname= "raj";
+// let lname= "rai";
+
+// console.log(`${fname} ${lname}`);
+
+// var str= "                            raj rai                  ";
+
+// console.log(str);
+// console.log(str.trim())
+
+
+// console.log(new Date().toLocaleString());
+// new Date(Year, month, day, hours, minutes, seconds, milliseconds);
+
+// console.log(Date.now());
+
+// console.log(new Date());
+
+// console.log(Math.PI);
+
+// Objects 
+
+// let bioData= {
+//   myName : "Raj",
+//   myAge : 21,
+//   getData(){
+//     console.log(`my name is ${bioData.myName} and my age is ${bioData.myAge}`);
+//   },
+// }
+
+// bioData.getData();
+
+// let myNames = 'vinod';
+// function myName() {
+//   console.log(this.myNames);
+// }
+// myName();
+
+const obj =  {
+  myAge: 21,
+  myName :() => {
+    console.log(this);
+  },
+}
+
+obj.myName();
