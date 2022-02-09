561c277604ff01c7d2413a0b2a4562657c65aaa6
Raj Rai <rrai1590@gmail.com>
2022-02-09 18:20:46 +0530

    more on js

--- practice/practice.js
+++ practice/practice.js
@@ -106,11 +106,145 @@
 // }
 // myName();
 
-const obj =  {
-  myAge: 21,
-  myName :() => {
-    console.log(this);
-  },
-}
+// const obj =  {
+//   myAge: 21,
+//   myName :() => {
+//     console.log(this);
+//   },
+// }
 
-obj.myName();
+// obj.myName();
+// let myName= "Raj";
+// let myBioData= {
+//   [myName] : "Hello How are you?  ",
+//   // [myLname] : 'Rai',
+//   // 21 : "is my age",
+//   // myDegree : 'btech'
+// };
+
+// console.log(myBioData);
+
+// for(let i=1 ;i<=20 ;i++){
+//   if(i%3==0 && i%5==0) console.log(i," : FizzBuzz");
+//   else if(i%3==0) console.log(i,": Fizz");
+//   else if (i%5==0) console.log(i,": Buzz");
+//   else console.log(i);
+// }
+
+// let low=1, high=10;
+// for(let num =low ; num<=high;num++){
+//   let flag=false;
+//   for(let a=2;a<num;a++){
+//     if(num%a==0) {
+//      flag=true;
+//      break; 
+//     }
+//   }
+//   if(!flag) console.log(num);
+// }
+
+// function fib (n){
+//   if(n==0 || n==1) return 1;
+//   return fib(n-1) + fib(n-2); 
+// }
+
+// const num= fib(5);
+// console.log(num);
+// let string = "";
+// for ( let num=0; num<=5; num++){
+//   for(let space = 5-num-1; space>=1; space--) string += " ";
+//   for(let star = 0; star<= num;star++) string += "*";
+//   string +="\n";
+// }
+
+// console.log(string);
+
+// let num =13;
+// let bin =0, i=1;
+// while(num>=1){
+//   let y=num%2;
+//   bin = bin + y*i 
+//   num=parseInt(num/2);
+//   i=i*10;
+// }
+// console.log(bin);
+
+// let arr = [1,2,3,4,5];
+// let narr = arr.reverse();
+// console.log(narr);
+
+// let arr1 = [1,2,3];
+// let arr2= [100,2,1,10];
+// let output;
+// while (arr1.length){
+//   output.push(arr1.pop());
+// }
+// while(arr2.length){
+//   output.push(arr2pop());
+// }
+// console.log(output);
+// function union ( arr1, arr2)
+// {
+// let obj ={};
+// for(let num = arr1.length-1; num>=0; num--) obj[arr1[num]]= arr1[num];
+// for(let num = arr2.length-1; num>=0; num--) obj[arr1[num]]= arr2[num];
+// let res=[];
+// for( var n in obj){
+//   if(obj.hasOwnProperty(n))
+//     res.push(obj[n]);
+// }
+// return res
+// }
+// console.log(union([1,2,3], [100,2,1,10]));
+
+// function abc() {
+//   if(1){
+//   var a=5;
+//   let b= 6;
+//   const c = 7;
+//   }
+//   console.log(a)
+//   console.log(b)
+//   console.log(c)
+
+// }
+// abc();
+
+// console.log(arr);
+// let i= 0;
+// while(i<arr.length){
+  //   console.log(`elem at index at i : ${arr[i]}`);
+  //   i++;
+  // }
+  
+  // arr.push(6);
+  // arr.unshift(0);
+  // console.log(arr);
+  
+// let arr = [1,2,3,4,5];
+// let arr1=arr.splice(2,10);
+// console.log(arr1);
+
+
+// for(let i = 0; i<3;i++){
+//   setTimeout(()=>console.log(i))
+// }
+
+// function union (arr1, arr2){
+//   let res=[];
+//   let obj={};
+//   for(let i=arr1.length-1;i>=0;i--) obj[arr1[i]]=arr1[i];
+//   for(let i=arr2.length-1;i>=0;i--) obj[arr2[i]]=arr2[i];
+//   for(let n in obj){
+//     res.push(obj[n]);
+//   }
+//   return res;
+// }
+// console.log(union([1,2,3], [100,2,1,10]));
+
+function sum (arr) {
+  return arr.reduce((a,b) => a+b,0)
+};
+
+var res= sum(arr1) + sum(arr2);
+console.log(res);
---
+++ practice/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+= 0;
+// while(i<arr.length){
+//   console.log(`elem at index at i : ${arr[i]}`);
+//   i++;
+// }
