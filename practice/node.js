27de9375492ddbdc71ddaaf65d7e170209698c36
Raj Rai <rrai1590@gmail.com>
2022-02-18 12:58:38 +0530

    file syn and write sync

--- practice/node.js
+++ practice/node.js
@@ -12,3 +12,20 @@
 // let buffer = fs.readFileSync("practice.js");
 // console.log(""+buffer);
 // f
+
+// console.log("varname", varname);
+// var varname;
+// console.log("varname", varname);
+// varname="captain America"
+// console.log("varname", varname);
+// fn();
+// function fn(){
+//   console.log("Hello from fn");
+// }
+// fn();
+// fnContainer();
+// var fnContainer = function(){
+//   console.log("I am a Expression");
+// }
+// fnContainer();
+
